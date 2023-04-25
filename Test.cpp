6c20cf1931d3fceb82a74cad0ec2525950b2fcdf
LidorKY <118388214+LidorKY@users.noreply.github.com>
2023-04-25 13:25:28 +0300

    create normal tests.

---
+++ Test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include "doctest.h"
+#include "sources/Fraction.hpp"
+using namespace std;
+using namespace ariel;
+
+TEST_CASE("name it with something"){
+    Fraction a (1,2), b(3,4);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+    CHECK(a+b == a);
+}
+
+
+
